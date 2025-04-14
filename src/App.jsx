74e960e85a9474ccc4ccd4f65c1db6d76081d993
Samiksha <samiksha.gnawali@selu.edu>
2025-04-13 19:00:53 -0500

    created home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
+import HomePage from './pages/homePage' 
 
 function App() {
   
   return (
     <div>
     <Navbar/>
+    <HomePage/>
     <Footer/>
+    
     </div>
   )
 }
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,6 @@
+function HomePage() {
+    return (
+        <div>This is home page.</div>
+    );
+}
+export default HomePage;
